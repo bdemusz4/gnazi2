a63f5a97a0271363c4a84913fc562eee4736a512
Jayson Saavedra <jayson.saavedra@solium.com>
2018-09-26 17:19:39 -0600

    fixed

--- index.js
+++ index.js
@@ -1,5 +1,10 @@
 const { CommandoClient } = require('discord.js-commando')
 const path = require('path')
+const MongoClient = require('mongodb').MongoClient
+const axios = require('axios')
+const querystring = require('querystring')
+const options = { useNewUrlParser: true }
+const grammarCheckAPI = 'https://languagetool.org/api/v2/check'
 require('dotenv').config()
 
 const client = new CommandoClient({
@@ -17,9 +22,55 @@ client.registry
     .registerDefaultCommands()
     .registerCommandsIn(path.join(__dirname, 'commands'))
 
-client.on('ready', () => {
-    console.log('Logged in!')
-    client.user.setActivity('Correcting grammar. $$help')
+MongoClient.connect(process.env.URL, options, (err, dbClient) => {
+    if (err) throw new Error(err)
+    const db = dbClient.db('servers')
+    const collection = db.collection('server')
+    
+    client.on('ready', () => {
+        console.log('Logged in!')
+        client.user.setActivity('Correcting grammar. $$help')
+    })
+
+    client.on('message', async message => {
+        if(message.author.bot) return
+        const guild = await collection.findOne({ server_id: { $eq: message.guild.id }})
+        await console.log(guild, message.guild.id)
+        if (guild === null) return
+
+        message.reply(checkGrammar(message.content, guild))
+    })
+
+    client.on('messageReactionAdd', async messageReaction => {
+        if (messageReaction.emoji.name !== 'grammar') return
+        const guild = await collection.findOne({ server_id: { $eq: messageReaction.message.guild.id }})
+        if(guild === null) return 
+
+        messageReaction.message.reply(checkGrammar(messageReaction.message.content, guild))
+    })
+
+    client.login(process.env.TOKEN)
 })
 
-client.login(process.env.TOKEN)
+const checkGrammar = async (message, guild) => {
+    let count = 1, response
+
+    if(Math.floor(Math.random() * 100) <= guild.frequency) {
+
+        try {
+            response = await axios.post(grammarCheckAPI, querystring.stringify({
+                'text': message,
+                'language': guild.langugae,
+                'disabledRules': guild.disabledRules
+            }))    
+        } catch(err) {
+            console.log(err)
+        }
+        
+        if (response.data.matches.length !== 0) {
+            return response.data.matches.map(match => {
+                return `${count++}. ${match.message}.\n`
+            })
+        }
+    }
+}
